2abc519d58baaccc1056e65d6186550f30f0295d
Carlos Hugo Gonzalez Castell (fundar) <carlos@fundar.org.mx>
2012-10-16 15:51:25 -0500

    quitando iconos de redes sociales

--- www/lib/themes/razones/footer.php
+++ www/lib/themes/razones/footer.php
@@ -1,19 +1,6 @@
 			<div class="clear"></div>
 			
-		
-			
 			<div id="footer">
-				<div class="social">
-					<a class=" twitter" href="http://twitter.com/FundarMexico" title="Fundar Twitter">
-						<img src="<?php print $this->themePath; ?>/css/images/twitter.png" alt="Fundar Twitter" />
-					</a>
-					<a class=" facebook" href="http://www.facebook.com/FundarMexico" title="Fundar Facebook">
-						<img src="<?php print $this->themePath; ?>/css/images/facebook.png" alt="Fundar Facebook" />
-					</a>
-					<a class=" youtube" href="http://www.youtube.com/fundarac" title="Fundar Youtube">
-						<img src="<?php print $this->themePath; ?>/css/images/youtube.png" alt="Fundar Youtube" />
-					</a>
-				</div>
 				<a href="http://fundar.org.mx" title="Fundar.org.mx">
 					<img src="<?php print $this->themePath; ?>/css/images/fundar.jpg" alt="Fundar.org.mx" />
 				</a>
